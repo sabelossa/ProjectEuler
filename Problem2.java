960ead4e40c731cf5419378531ced3d0e1979460
sabelossa <58365597+sabelossa@users.noreply.github.com>
2020-09-09 18:28:45 +0100

    incomplete problem2

---
+++ Problem2.java
@@ -0,0 +1,38 @@
+
+public class Problem2 {
+
+		//INCOMPLETE
+	
+	public static void Problem2Method() {
+		
+		
+		
+		
+	}
+	
+	
+	public static void Fib() {
+		
+		
+		
+		System.out.println(Fib(4));
+		
+	}
+	
+	public static int Fib(int n) {
+		
+		
+		
+		if (n == 0) {
+			return 1;
+		}
+		if (n == 1) {
+			
+			return 1;
+		}
+		else {
+			return Fib(n-1) + Fib(n-2);
+		}
+	}
+	
+}
